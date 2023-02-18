f0a2359549e8e3f794ebec743f068918aa6750ef
Sarab1324 <singhsarab166@gmail.com>
2023-02-18 18:39:28 +0530

    'initial commit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { useState } from "react";
 function App() {
+  //   const [toggle, setToggle] = useState(true)
+  //   const [name, setname] = useState('Sarab')
+  //   const toggleName = ()=> {
+  //    setToggle(!toggle)
+  //   }
+  //   const changeName = (param) =>  {
+  //  setname(param)
+  //   }
+  const [gallery, setGallery] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const [author, img] = e.target;
+    const newData = {
+      author: author.value,
+      img: img.value,
+    };
+    setGallery([...gallery, newData]);
+  };
+  let show = <h1>no content</h1>;
+  if(gallery.length > 0 ){
+    show = gallery.map((e, i) => (
+     <li key={i}>
+       {e.author} : <img width={100} src={e.img} alt="no img" />
+     </li>
+   ));
+
+  }
+  console.log(show);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* {toggle ? <h1>My toggle is Sarab</h1> : 'loading'}  
+      <button onClick={toggleName}>Correct the toggle</button>
+
+      {name}  
+      <button onClick={() => changeName('Master')}>change name</button> */}
+
+      <form className="w-100" onSubmit={submitHandler}>
+        <input className="form-control w-25" type="text" placeholder="author" />
+        <input
+          className="form-control w-25"
+          type="url"
+          placeholder="enter img url"
+        />
+        <button className="btn btn-primary w-25"> Add</button>
+      </form>
+      <hr />
+      <ol>{show}</ol>
+    </>
   );
 }
 
